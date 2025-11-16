866ed3ec46796655729551780159f68fba65f585
VeganMeatGuy <steudel.peter@gmail.com>
2025-11-16 16:42:47 +0100

    feature: feature-site; add caption, displayed value (startet by 1) and button, if button is clicked -1 is added to displayed value

--- AngularTestApp/src/app/presentation/views/features/features.ts
+++ AngularTestApp/src/app/presentation/views/features/features.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component,OnInit} from '@angular/core';
+import { FormControl} from '@angular/forms';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-features',
@@ -6,6 +8,18 @@ import { Component } from '@angular/core';
   templateUrl: './features.html',
   styleUrl: './features.scss',
 })
-export class Features {
 
-}
+export class Features implements OnInit {
+
+  counterValue = 0;
+  counterText = this.counterValue.toString
+
+	ngOnInit(): void {
+    this.counterValue = 1
+    	}
+
+  	async addOne() {
+        this.counterValue = this.counterValue + 1;
+		}
+	}
+
